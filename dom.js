e29d6e5ec2aa283e3bda126f90480e278e37aa1b
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-22 19:08:09 +0530

    6th queryselectorall

--- dom.js
+++ dom.js
@@ -4,9 +4,9 @@ header.style.borderBottom = 'solid 3px black';
 // const ul = document.getElementById('items');
 // ul.style.color = "green";
 
-const heading = document.getElementsByClassName('title');
+// const heading = document.getElementsByClassName('title');
 
-heading[0].style.fontWeight = 900;
+// heading[0].style.fontWeight = 900;
 
 const items = document.getElementsByClassName('list-group-item');
 
@@ -22,9 +22,20 @@ for(let i=0;i<items.length;i++)
 // tagItems[4].style.color = "yellow";
 
 //Second list element colour changed to green
-let secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
 
-//Second list element visibility changed to hidden
-let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility = 'hidden';
+// //Second list element visibility changed to hidden
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden';
+
+//font colour change to green for second element
+let listQueries = document.querySelectorAll('.list-group-item');
+listQueries[1].style.color = 'green';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odd.length ;i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
