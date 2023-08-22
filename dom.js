d787d339cc0a2d63212b78c7c22dc6c454d638f8
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-22 12:13:28 +0530

    third commit

--- dom.js
+++ dom.js
@@ -3,3 +3,7 @@ header.style.borderBottom = 'solid 3px black';
 
 const ul = document.getElementById('items');
 ul.style.color = "green";
+
+// const heading = document.getElementsByClassName('title');
+
+// heading.style.fontWeight = 100;
