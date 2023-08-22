cee801ff11a2a41386973c39789383837333125e
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-22 12:36:37 +0530

    third commit

--- dom.js
+++ dom.js
@@ -1,9 +1,18 @@
 const header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px black';
 
-const ul = document.getElementById('items');
-ul.style.color = "green";
+// const ul = document.getElementById('items');
+// ul.style.color = "green";
 
-// const heading = document.getElementsByClassName('title');
+const heading = document.getElementsByClassName('title');
 
-// heading.style.fontWeight = 100;
+heading[0].style.fontWeight = 900;
+
+const items = document.getElementsByClassName('list-group-item');
+
+items[2].style.color= "green";
+
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight=900;
+}
