86f04821626fbdfd45b60fd28a827565344c088a
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-23 19:59:45 +0530

    9th filter elements

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
+
 
 //form submit event
 form.addEventListener('submit', addItem);
@@ -7,13 +9,16 @@ form.addEventListener('submit', addItem);
 //delete event listener
 itemList.addEventListener('click',removeItem);
 
+//filter event listener
+filter.addEventListener('keyup',filterItems);
+
 //Add item
 function addItem(e){
     e.preventDefault();
   
     //Get input value
     let newItem =document.getElementById('item').value;
-
+    let newDesc = document.getElementById('description').value;
     //create new li element
     let li = document.createElement('li');
 
@@ -21,7 +26,7 @@ function addItem(e){
     li.className ='list-group-item';
 
     //add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem + "" + newDesc));
 
     //create del button element
     let deleteBtn = document.createElement('button');
@@ -57,4 +62,25 @@ function removeItem(e){
         }
     }
     
+}
+
+//Filter Items
+
+function filterItems(e){
+    //convert text to lowercase
+    let text = e.target.value.toLowerCase();
+    //get list items
+    let items = document.getElementsByTagName('li');
+    //convert to an array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) !=-1)
+        {
+            item.style.display = 'block';
+        }
+        else
+        {
+            item.style.display= 'none';
+        }
+    })
 }
